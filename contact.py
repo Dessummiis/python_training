28d85cc9a752db9a2aa72021212a95c14dea170b
Sergey <s.dobrovolskiy@rtk-dc.ru>
2019-09-18 09:28:48 +0300

    Create contact.py
    Комит выделенного вспомогательного класса

---
+++ contact.py
@@ -0,0 +1,22 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, lastname, nickname, title, company, address, phone_home, phone_mobile,
+                                 phone_work, phone_fax, email_1, email_2, email_3, homepage, secondary_address, secondary_phone, secondary_notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.phone_home = phone_home
+        self.phone_mobile = phone_mobile
+        self.phone_work = phone_work
+        self.phone_fax = phone_fax
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.secondary_address = secondary_address
+        self.secondary_phone = secondary_phone
+        self.secondary_notes = secondary_notes
