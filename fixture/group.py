300d05238ace17b126adc1ea44586b724aba402e
Sergey <dessummiis@users.noreply.github.com>
2019-10-05 13:04:15 +0300

    Оптимизирован переход на страницу групп

--- fixture/group.py
+++ fixture/group.py
@@ -3,9 +3,10 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-    def return_to_groups_page(self):
+    def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
         wd = self.app.wd
@@ -15,7 +16,7 @@ class GroupHelper:
         self.fill_group_fields(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
+        self.open_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -24,11 +25,7 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # submit deletion
         wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        self.open_groups_page()
 
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
@@ -39,7 +36,7 @@ class GroupHelper:
         self.fill_group_fields(new_group_data)
         # submit group creation
         wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
+        self.open_groups_page()
 
     def select_first_group(self):
         wd = self.app.wd
