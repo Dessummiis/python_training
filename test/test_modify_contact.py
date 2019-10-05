1a65e52b823a211a61b3979aa3650edb86282e1c
Sergey <dessummiis@users.noreply.github.com>
2019-10-05 12:49:03 +0300

    Модификация групп\контактов при их отсутсвии

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,6 +2,8 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="test"))
     app.contact.modify_first_contact(
         Contact(first_name="edit_fname", middle_name="edit_mname", lastname="edit_lnmame", nickname="edit_nickname",
                 title="edit_title", company="edit_company", address="edit_addr1",
@@ -12,5 +14,7 @@ def test_modify_contact(app):
 
 
 def test_modify_fist_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="test"))
     app.contact.modify_first_contact(
         Contact(first_name="fname_new"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,7 +4,11 @@ from model.group import Group
 #     app.group.modify_first_group(Group(name="edit_group_name", header="edit_group_header", footer="edit_group_footer"))
 
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="New group"))
 
 def test_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="New header"))
