e9346381b7f1458e6ebee53bb7b1c4c96064f978
Sergey <dessummiis@users.noreply.github.com>
2019-10-05 09:59:47 +0300

    Выделен метод для заполнения полей группы

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,5 @@
+from model.group import Group
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -12,16 +14,7 @@ class GroupHelper:
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_fields(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -46,7 +39,13 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # edit group
         wd.find_element_by_name("edit").click()
-        # fill group form
+        self.fill_group_fields(group)
+        # submit group creation
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def fill_group_fields(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -56,7 +55,3 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
-
