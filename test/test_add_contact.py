e265dad22d4d16eccc763274928e26b3cd9abae4
Sergey <dessummiis@users.noreply.github.com>
2019-10-06 15:02:16 +0300

    Добавлена проверка равенства объектов и списков у групп

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,8 +4,10 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
+    old_contacts = app.contact.get_contact_list()
     app.contact.create(
         Contact(first_name="fname", middle_name="mname", lastname="lnmame", nickname="nickname", title="title",
                 company="company", address="addr1", phone_home="th", phone_mobile="tm",
                 phone_work="tw", phone_fax="fax", email_1="e1", email_2="e2", email_3="e3", homepage="h",
                 secondary_address="addr2", secondary_phone="home2", secondary_notes="notes"))
+    new_contacts
