edd8682327abe2ab05fdab7725b1f08e8e789c89
Sergey <dessummiis@users.noreply.github.com>
2019-10-06 18:13:16 +0300

    4_3 добавлен assert для модификации контакта

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,15 +5,18 @@ def test_modify_contact(app):
     old_contacts = app.contact.get_contact_list()
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="test"))
-    app.contact.modify_first_contact(
-        Contact(first_name="edit_fname", middle_name="edit_mname", last_name="edit_lnmame", nickname="edit_nickname",
+    contact = Contact(first_name="edit_fname", middle_name="edit_mname", last_name="edit_lnmame", nickname="edit_nickname",
                 title="edit_title", company="edit_company", address="edit_addr1",
                 phone_home="edit_th", phone_mobile="edit_tm", phone_work="edit_tw", phone_fax="edit_fax",
                 email_1="edit_e1", email_2="edit_e2", email_3="edit_e3",
                 homepage="edit_h", secondary_address="edit_addr2", secondary_phone="edit_home2",
-                secondary_notes="edit_notes"))
+                secondary_notes="edit_notes")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 # def test_modify_fist_name(app):
