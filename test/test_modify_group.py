ec17b43bb2fb121391c06cae77e3a1ccfd5df2ba
Sergey <dessummiis@users.noreply.github.com>
2019-10-06 14:49:46 +0300

    Добавлена проверка на удаление группы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,11 +4,17 @@ from model.group import Group
 #     app.group.modify_first_group(Group(name="edit_group_name", header="edit_group_header", footer="edit_group_footer"))
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
