339b61ed7a5b345fc565bc375ea88fef471498dd
Sergey <dessummiis@users.noreply.github.com>
2019-10-06 15:47:37 +0300

    Update test_add_contact.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,4 +10,5 @@ def test_add_contact(app):
                 company="company", address="addr1", phone_home="th", phone_mobile="tm",
                 phone_work="tw", phone_fax="fax", email_1="e1", email_2="e2", email_3="e3", homepage="h",
                 secondary_address="addr2", secondary_phone="home2", secondary_notes="notes"))
-    new_contacts
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
