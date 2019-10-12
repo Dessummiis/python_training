72ca298678be6efebe1d2182efc405dc915891c4
Sergey <dessummiis@users.noreply.github.com>
2019-10-12 18:30:20 +0300

    6_4 wip

--- generator/contact.py
+++ generator/contact.py
@@ -1,23 +1,25 @@
 from model.contact import Contact
 import random
 import string
-
-
-constant = [
-    Contact(first_name="first_name1", middle_name="middle_name1", last_name="last_name1",
-            address="address1", phone_home="phone_home1", phone_mobile="phone_mobile1",
-            phone_work="phone_work1", secondary_phone="secondary_phone1",
-            email_1="email_11", email_2="email_21", email_3="email_21"),
-    Contact(first_name="first_name2", middle_name="middle_name2", last_name="last_name2",
-            address="address2", phone_home="phone_home2", phone_mobile="phone_mobile2",
-            phone_work="phone_work2", secondary_phone="secondary_phone2",
-            email_1="email_12", email_2="email_22", email_3="email_22")
-]
+import os.path
+import json
+import getopt
+import sys
 
 
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
 
 
 testdata = [Contact(first_name="", middle_name="",last_name="", address="", phone_home="", phone_mobile="",
@@ -27,5 +29,10 @@ testdata = [Contact(first_name="", middle_name="",last_name="", address="", phon
             phone_home=random_string("phone_home", 10), phone_mobile=random_string("phone_mobile", 10),
             phone_work=random_string("phone_work", 10), secondary_phone=random_string("secondary_phone", 10),
             email_1=random_string("email_1", 10), email_2=random_string("email_2", 10), email_3=random_string("email_3", 10))
-    for i in range(5)
+    for i in range(n)
 ]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as f:
+    f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
