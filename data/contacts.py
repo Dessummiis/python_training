f2f1b275e84371b81fbf7afa6f618b73ba5aa1e1
Sergey <dessummiis@users.noreply.github.com>
2019-10-12 19:58:23 +0300

    6_5

---
+++ data/contacts.py
@@ -0,0 +1,13 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(first_name="first_name1", middle_name="middle_name1", last_name="last_name1",
+            address="address1", phone_home="phone_home1", phone_mobile="phone_mobile1",
+            phone_work="phone_work1", secondary_phone="secondary_phone1",
+            email_1="email_11", email_2="email_21", email_3="email_21"),
+    Contact(first_name="first_name2", middle_name="middle_name2", last_name="last_name2",
+            address="address2", phone_home="phone_home2", phone_mobile="phone_mobile2",
+            phone_work="phone_work2", secondary_phone="secondary_phone2",
+            email_1="email_12", email_2="email_22", email_3="email_22")
+]
