8988873b19eee38ac1d0c438f74363fe14dc9b46
Sergey <dessummiis@users.noreply.github.com>
2019-10-12 20:27:38 +0300

    6_5 модификация групп\контактов обновлена для работы с  тестовыми данными в json

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,15 +2,10 @@ from model.contact import Contact
 from random import randrange
 
 
-def test_modify_contact(app):
+def test_modify_contact(app, json_contacts):
+    contact = json_contacts
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="test"))
-    contact = Contact(first_name="edit_fname", middle_name="edit_mname", last_name="edit_lnmame", nickname="edit_nickname",
-                title="edit_title", company="edit_company", address="edit_addr1",
-                phone_home="edit_th", phone_mobile="edit_tm", phone_work="edit_tw", phone_fax="edit_fax",
-                email_1="edit_e1", email_2="edit_e2", email_3="edit_e3",
-                homepage="edit_h", secondary_address="edit_addr2", secondary_phone="edit_home2",
-                secondary_notes="edit_notes")
     old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
     contact.id = old_contacts[index].id
@@ -19,10 +14,3 @@ def test_modify_contact(app):
     new_contacts = app.contact.get_contact_list()
     old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-# def test_modify_fist_name(app):
-#     if app.contact.count() == 0:
-#         app.contact.create(Contact(first_name="test"))
-#     app.contact.modify_first_contact(
-#         Contact(first_name="fname_new"))
