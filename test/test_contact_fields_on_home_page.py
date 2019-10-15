ec6e6a0b462e60b8f50c9dd5d01d6f0d5855b37c
Sergey <dessummiis@users.noreply.github.com>
2019-10-15 11:33:44 +0300

    7_21 fix WIP

--- test/test_contact_fields_on_home_page.py
+++ test/test_contact_fields_on_home_page.py
@@ -1,11 +1,21 @@
-from random import randrange
+from model.group import Group
+from model.contact import Contact
 
 
-def test_contact_fields_on_home_page(app):
-    contacts = app.contact.get_contact_list()
-    index = randrange(len(contacts))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    contact_from_edit_page.all_phones = app.contact.merge_phones_like_on_home_page(contact_from_edit_page)
-    contact_from_edit_page.all_emails = app.contact.merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page == contact_from_edit_page
+def test_contact_fields_on_home_page(app, orm, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(first_name="test"))
+    contacts_from_home_page = app.contact.get_contact_list()
+    contacts_from_db = db.get_contact_list()
+    for i in contacts_from_db:
+        contacts_from_home_page[i].all_phones = app.contact.merge_phones_like_on_home_page(i)
+        contacts_from_home_page[i].all_emails = app.contact.merge_emails_like_on_home_page(i)
+    lol = 0
+    # checklist = []
+    # for contact in contacts_from_db:
+    #     if contact in contacts_from_home_page:
+    #         checklist.append(True)
+    #     else:
+    #         checklist.append(False)
+    # print (checklist)
+    # assert sorted(contact_from_home_page, key=Group.id_or_max) == sorted(contact_from_db, key=Group.id_or_max)
--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -11,5 +11,6 @@ from model.group import Group
 
 def test_contact_list(app, db):
     ui_list = app.contact.get_contact_list()
-    db_list = map(app.contact.clean, db.get_contact_list())
+    db_list = db.get_contact_list()
+    # db_list = map(app.contact.clean, db.get_contact_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
