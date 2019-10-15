1cdf9ded1ecb2164b22f64230d953b379ffedec7
Sergey <dessummiis@users.noreply.github.com>
2019-10-15 10:14:38 +0300

    Revert "7_21 перенос из db_matches_ui в contact_fields_on_home_page"
    This reverts commit e46b4d17e7478c650e6fbd044e1b4981d8f10a62.

--- test/test_contact_fields_on_home_page.py
+++ test/test_contact_fields_on_home_page.py
@@ -1,7 +1,11 @@
-from model.group import Group
+from random import randrange
 
 
-def test_contact_list(app, db):
-    ui_list = app.contact.get_contact_list()
-    db_list = map(app.contact.clean, db.get_contact_list())
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+def test_contact_fields_on_home_page(app):
+    contacts = app.contact.get_contact_list()
+    index = randrange(len(contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    contact_from_edit_page.all_phones = app.contact.merge_phones_like_on_home_page(contact_from_edit_page)
+    contact_from_edit_page.all_emails = app.contact.merge_emails_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page == contact_from_edit_page
