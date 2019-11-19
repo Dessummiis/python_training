5718fbbd713b1471113d8c8356afa1cff68defa7
Sergey <s.dobrovolskiy@rtk-dc.ru>
2019-11-19 12:54:29 +0300

    21 wip

--- test/test_contact_fields_on_home_page.py
+++ test/test_contact_fields_on_home_page.py
@@ -5,17 +5,15 @@ from model.contact import Contact
 def test_contact_fields_on_home_page(app, orm, db):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(first_name="test"))
-    contacts_from_home_page = app.contact.get_contact_list()
-    contacts_from_db = db.get_contact_list()
-    # for i in contacts_from_db:
-    #     contacts_from_home_page[i].all_phones = app.contact.merge_phones_like_on_home_page(i)
-    #     contacts_from_home_page[i].all_emails = app.contact.merge_emails_like_on_home_page(i)
-    smth = 0
-    # checklist = []
-    # for contact in contacts_from_db:
-    #     if contact in contacts_from_home_page:
-    #         checklist.append(True)
-    #     else:
-    #         checklist.append(False)
-    # print (checklist)
-    # assert sorted(contact_from_home_page, key=Group.id_or_max) == sorted(contact_from_db, key=Group.id_or_max)
+    # получить сортированные списки из ui и db
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    # проверить на соответствие длины списков контактов в ui и db
+    assert len(contacts_from_home_page) == len(contacts_from_db)
+    # использовать PEP 279 для индексации списка
+    for index, contact in enumerate(contacts_from_db):
+        # склеить номера телефонов и email-ы для контактов из db
+        contacts_from_db[index].all_phones = app.contact.merge_phones_like_on_home_page(contact)
+        contacts_from_db[index].all_emails = app.contact.merge_emails_like_on_home_page(contact)
+        # сравнить контакт из db с контактом из ui для соответствующего индекса
+        assert contacts_from_db[index] == contacts_from_home_page[index]
