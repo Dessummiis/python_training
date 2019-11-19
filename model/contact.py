54d70b8bb381786eab121112bbcdb1458a347123
Sergey <s.dobrovolskiy@rtk-dc.ru>
2019-11-19 13:44:57 +0300

    21 fin

--- model/contact.py
+++ model/contact.py
@@ -7,7 +7,7 @@ class Contact:
                  company=None, address=None, phone_home=None, phone_mobile=None, phone_work=None,
                  phone_fax=None, email_1=None, email_2=None, email_3=None, homepage=None,
                  secondary_address=None, secondary_phone=None, secondary_notes=None,
-                 all_phones=None, all_emails=None):
+                 all_phones=None, all_emails=None, full_assert=None):
         self.id = id
         self.name = name
         self.first_name = first_name
@@ -30,13 +30,23 @@ class Contact:
         self.secondary_notes = secondary_notes
         self.all_phones = all_phones
         self.all_emails = all_emails
+        self.full_assert = full_assert
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.first_name, self.last_name)
+        return "%s:%s:%s%s%s%s" % \
+               (self.id, self.first_name, self.last_name, self.address, self.all_emails, self.all_phones)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) \
-               and self.first_name == other.first_name and self.last_name == other.last_name
+        # Проверка по имени и фамилии, без учета id
+        if self.full_assert and other.full_assert is None:
+            return (self.id is None or other.id is None or self.id == other.id) \
+                   and self.first_name == other.first_name and self.last_name == other.last_name
+        else:
+            # Проверка по всем полям со страницы контактов, без учета id
+            return (self.id is None or other.id is None or self.id == other.id) \
+                   and self.first_name == other.first_name and self.last_name == other.last_name \
+                   and self.address == other.address and self.all_phones == other.all_phones \
+                   and self.all_emails == other.all_emails
 
     def id_or_max(gr):
         if gr.id:
